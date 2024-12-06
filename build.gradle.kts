e8a2c32c11bdf051dd1fbc25b1b3912c3bca655b
Matheus <matheus.pmc@hotmail.com>
2024-12-05 23:00:14 -0300

    Init

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
+	id("org.springframework.boot") version "3.4.0"
+	id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "com.payment"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.springframework.boot:spring-boot-starter-json")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation(platform("io.awspring.cloud:spring-cloud-aws-dependencies:3.0.4"))
+	implementation("io.awspring.cloud:spring-cloud-aws-starter-sqs")
+	runtimeOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8")
+
+	implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
